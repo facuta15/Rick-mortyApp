887ccb23356a41fb3577de2442b644d130afc312
facundotadini-admin <facundo@activity.com.ar>
2023-08-10 15:10:07 -0300

    Added routes and navigation

---
+++ src/app/models/routes.model.ts
@@ -0,0 +1,5 @@
+export enum Routes {
+    HOME ="/",
+    CHARACTERS = "information/characters",
+    LOCATIONS = "information/locations",
+ }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
-import DashboardLayout from './dashboard/layout'
-
+import React from "react";
+import DashboardLayout from "./dashboard/layout";
+import Link from "next/link";
+import { Routes } from "./models";
+import { Button } from "@mui/material";
 function App() {
   return (
     <div>
       <h1>Welcome home</h1>
       <h2>Let's begin</h2>
+      <div>
+        <Button>
+          <Link href={Routes.CHARACTERS}>Characters</Link>
+        </Button>
+        <Button>
+          <Link href={Routes.LOCATIONS}>Locations</Link>
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
